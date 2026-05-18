9b7bd34455e29a127ee60d27656f5b3b5faff08d
dasom <165390070+s0mmcc0@users.noreply.github.com>
2026-05-18 15:54:12 +0900

    chore: VS Code 기반 초기 스프링 부트 뼈대 프로젝트 생성 close #1

---
+++ src/main/java/com/routedraft/RouteDraftApplication.java
@@ -0,0 +1,13 @@
+package com.routedraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteDraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routedraft/RouteDraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routedraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteDraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
